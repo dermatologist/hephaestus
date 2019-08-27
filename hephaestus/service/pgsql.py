fbd0c70ceaaa2671723f2ebd9ce86b764fdc8067
dermatologist <github@gulfdoctor.net>
2019-08-27 16:38:07 -0400

    feat: Add nonspecific schema writer for pgsql

--- hephaestus/service/pgsql.py
+++ hephaestus/service/pgsql.py
@@ -13,6 +13,19 @@ Used by load-> init and transform init
 """
 
 
+def get_schema_engine(vocab):
+    # https://stackoverflow.com/questions/9298296/sqlalchemy-support-of-postgres-schemas
+    dbschema = '{},public'  # Searches left-to-right
+    dbschema = dbschema.format(vocab)
+    pgsql_url = 'postgresql+psycopg2://{}:{}@{}:{}/{}'
+    pgsql_url = pgsql_url.format(C.CDM_USER_NAME, C.CDM_USER_PASS,
+                                 C.CDM_USER_HOST, C.CDM_USER_PORT, C.CDM_USER_DB)
+    engine = create_engine(
+        pgsql_url,
+        connect_args={'options': '-csearch_path={}'.format(dbschema)})
+    return engine
+
+
 def get_reader():
     # https://stackoverflow.com/questions/9298296/sqlalchemy-support-of-postgres-schemas
     dbschema = '{},public'  # Searches left-to-right
