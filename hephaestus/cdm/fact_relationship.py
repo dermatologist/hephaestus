7e1898f546dee79881ac04fb09c9031697401288
dermatologist <github@gulfdoctor.net>
2019-08-29 09:16:33 -0400

    fix: add table name to fact_relationship table

--- hephaestus/cdm/fact_relationship.py
+++ hephaestus/cdm/fact_relationship.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, String, Integer, Date, DateTime
+from sqlalchemy import Column, Integer
 from sqlalchemy.ext.automap import automap_base
 
 Base = automap_base()
 
 
 class Fact_relationship(Base):
+    __tablename__ = 'fact_relationship'
     domain_concept_id = Column(Integer)
     fact_id_1 = Column(Integer)
     domain_concept_id_2 = Column(Integer)
