e21c06d2e7e870e98f9179e826f782ca3339560e
beapen <github@gulfdoctor.net>
2019-08-31 08:15:45 -0400

    progress: DAD transform

--- hephaestus/transform/dad_transform.py
+++ hephaestus/transform/dad_transform.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from hephaestus.cdm.automap import Location, Person, Observation, Procedure_occurrence
+from hephaestus.cdm.automap import Location, Person, Observation, Procedure_occurrence, Provider
 from hephaestus.settings import LocalSettings as C
 
 
@@ -9,6 +9,7 @@ def transform(*args):
     person = Person()
     observation = Observation()
     procedure_occurence = Procedure_occurrence()
+    provider = Provider()
     currentYear = datetime.now().year
     for row in args:
         person.person_source_value = row[0]
@@ -26,6 +27,17 @@ def transform(*args):
             person.gender_concept_id = C.CDM_CONCEPT_MALE
         else:
             person.gender_concept_id = C.CDM_CONCEPT_FEMALE
+        print(person.person_id)
         yield person
         yield location
-        yield row[2]
+        yield row[153]
+
+
+"""
+TODO : person needs to be processed first to get the ID
+Use flush as described here: https://stackoverflow.com/questions/28242523/get-inserted-key-before-commit-session/28242570
+So in this case we need two transform files, first for person and then the next for others
+The transform will do only flush. add will be in load.
+
+
+"""
